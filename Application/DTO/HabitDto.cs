9c6119a1eac728b12a3c4749ffe5cc284f7e86ec
AlekseiSergeevich <alekseisuharev03@gmail.com>
2025-06-24 18:08:13 +0300

    изменил dto (убрал поля)

--- Application/DTO/HabitDto.cs
+++ Application/DTO/HabitDto.cs
@@ -6,7 +6,6 @@ namespace Application.DTO
     {
         public Guid Id { get; set; }
         public required string Name { get; set; }
-        public Guid? UserId { get; set; }
         public Guid? RecordsId { get; set; } = new();
         public Guid? CategoryId { get; set; }
     }
--- Application/DTO/UserDto.cs
+++ Application/DTO/UserDto.cs
@@ -8,7 +8,6 @@ namespace Application.DTO
         public required string FullName { get; set; }
         public required string Email { get; set; }
         public required string PhoneNumber { get; set; }
-        public string? PasswordHash { get; set; }
         public Guid? HabitsId { get; set; }
     }
 }
